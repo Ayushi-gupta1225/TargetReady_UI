d201684ca3eeb835567232f7cf27ff5f841db03a
anaghdeebug <anaghdeepak@gmail.com>
2024-06-17 22:55:53 +0530

    put navbar into home page component

--- React Workspace/src/App.jsx
+++ React Workspace/src/App.jsx
@@ -1,35 +1,10 @@
-// import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
-// import ContactUs from './pages/ContactUs';
-// import AboutUs from './pages/AboutUs';
-import planogramImage from './assets/icon.png';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav className="bg-[white] p-4 shadow-md">
-          <ul className="flex justify-between items-center">
-            <li className="flex items-center">
-            <img src={planogramImage} alt="Planogram" className="h-8 mr-2" /> {/* Insert the image */}
-            <Link to="/" className="text-black text-lg font-bold">Planogram Manager</Link>
-            </li>
-            <div className="flex space-x-4">
-              {/* <li><Link className="text-white text-lg" to="/contact-us">Contact Us</Link></li>
-              <li><Link className="text-white text-lg" to="/about-us">About Us</Link></li> */}
-            </div>
-          </ul>
-        </nav>
-        <div className="p-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/contact-us" element={<ContactUs />} />
-            <Route path="/about-us" element={<AboutUs />} /> */}
-          </Routes>
-        </div>
-      </div>
-    </Router>
+    <div>
+      <Home />
+    </div>
   );
 }
 
--- React Workspace/src/pages/Home.jsx
+++ React Workspace/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import axios from 'axios';
 import Planogram from '../components/Planogram';
 import Swal from 'sweetalert2';
+import planogramImage from '../assets/icon.png';
 
 const Home = () => {
   const [initialFormData] = useState({
@@ -20,7 +22,7 @@ const Home = () => {
   const [locations, setLocations] = useState([]);
   const [formData, setFormData] = useState(initialFormData);
 
-  const realLifeHeightCm = 44;
+  const realLifeHeightCm = 45;
   const realLifeWidthCm = 90;
 
   const [scalingFactorHeight, setScalingFactorHeight] = useState(1);
@@ -54,13 +56,6 @@ const Home = () => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-
-  // const handleImageChange = (e) => {
-  //   const file = e.target.files[0];
-  //   if (file) {
-  //     setFormData({ ...formData, productImage: URL.createObjectURL(file) });
-  //   }
-  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -127,12 +122,11 @@ const Home = () => {
     } catch (error) {
       Swal.fire({
         title: 'Placement unsuccessful',
-        // text: `Error: ${error.message}`,
         icon: 'error',
         timer: 2500, 
         showConfirmButton: false,
       }).then(() => {
-        window.location.reload(); // Refresh the page after the alert
+        window.location.reload(); 
       });
     }
   };
@@ -146,145 +140,151 @@ const Home = () => {
   };
 
   return (
-    <div className="flex h-auto overflow-hidden">
-      <div className="w-2/5 bg-white shadow-md rounded p-6 border border-gray-300 overflow-auto">
-        <h2 className="text-center text-2xl font-bold">Add Product</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="flex space-x-4">
-            <div className="flex-1 space-y-4">
-              <div>
-                <label className="block text-gray-700">Product Name:</label>
-                <input
-                  type="text"
-                  name="productName"
-                  value={formData.productName}
-                  onChange={handleChange}
-                  className="mt-1 block w-full px-2 py-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
-                  autoComplete="off"
-                  required
+    <Router>
+    <div>
+      <nav className="bg-white p-4 shadow-md">
+        <ul className="flex justify-between items-center">
+          <li className="flex items-center">
+            <img src={planogramImage} alt="Planogram" className="h-8 mr-2" />
+            <Link to="/" className="text-black text-lg font-bold">Planogram Manager</Link>
+          </li>
+          <div className="flex space-x-4"></div>
+        </ul>
+      </nav>
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={
+            <div className="flex h-auto overflow-hidden">
+              <div className="w-2/5 bg-white shadow-md rounded p-6 border border-gray-300 overflow-auto">
+                <h2 className="text-center text-2xl font-bold">Add Product</h2>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  <div className="flex space-x-4">
+                    <div className="flex-1 space-y-4">
+                      <div>
+                        <label className="block text-gray-700">Product Name:</label>
+                        <input
+                          type="text"
+                          name="productName"
+                          value={formData.productName}
+                          onChange={handleChange}
+                          className="mt-1 block w-full px-2 py-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                          autoComplete="off"
+                          required
+                        />
+                      </div>
+                      <div>
+                        <label className="block text-gray-700">Product ID:</label>
+                        <input
+                          type="text"
+                          name="productId"
+                          value={formData.productId}
+                          onChange={handleChange}
+                          className="mt-1 block w-full px-2 py-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                          autoComplete="off"
+                          required
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex space-x-4">
+                    <div className="flex-1">
+                      <label className="block text-gray-700">Product Height(cm):</label>
+                      <input
+                        type="number"
+                        name="height"
+                        value={formData.height}
+                        onChange={handleChange}
+                        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                        autoComplete="off"
+                        required
+                      />
+                    </div>
+                    <div className="flex-1">
+                      <label className="block text-gray-700">Product Width(cm):</label>
+                      <input
+                        type="number"
+                        name="width"
+                        value={formData.width}
+                        onChange={handleChange}
+                        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                        autoComplete="off"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <label className="block text-gray-700">Quantity:</label>
+                    <button
+                      type="button"
+                      onClick={handleDecrement}
+                      className="px-2 py-1 bg-gray-200 text-gray-700 rounded-md focus:outline-none"
+                    >
+                      -
+                    </button>
+                    <span>{formData.quantity}</span>
+                    <button
+                      type="button"
+                      onClick={handleIncrement}
+                      className="px-2 py-1 bg-gray-200 text-gray-700 rounded-md focus:outline-none"
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Shelf:</label>
+                    <select 
+                      type="number" 
+                      name="shelf" 
+                      value={formData.shelf || ''} 
+                      onChange={handleChange}
+                      className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                      autoComplete="off"
+                      required
+                      >
+                      <option value="" disabled hidden>Select a shelf</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label className="block text-gray-700">Section:</label>
+                    <select 
+                      type="number" 
+                      name="section" 
+                      value={formData.section || ''} 
+                      onChange={handleChange}
+                      className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                      autoComplete="off"
+                      required
+                      >
+                      <option value="" disabled hidden>Select a section</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                    </select>
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:border-blue-300">
+                    Place Product
+                  </button>
+                </form>
+              </div>
+              <div className="w-3/5 bg-white shadow-md rounded p-6 border border-gray-300 overflow-auto">
+                <Planogram
+                    products={products}
+                    locations={locations}
+                    scalingFactorHeight={scalingFactorHeight}
+                    scalingFactorWidth={scalingFactorWidth}
                 />
               </div>
-              <div>
-                <label className="block text-gray-700">Product ID:</label>
-                <input
-                  type="text"
-                  name="productId"
-                  value={formData.productId}
-                  onChange={handleChange}
-                  className="mt-1 block w-full px-2 py-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
-                  autoComplete="off"
-                  required
-                />
-              </div>
             </div>
-             {/* <div style={{ width: '150px', height: '140px' }} className="border border-gray-300 rounded-md flex items-center justify-center overflow-hidden">
-              {formData.productImage ? (
-                <img src={formData.productImage} alt="Product" className="object-cover w-full h-full" />
-              ) : (
-                <label className="cursor-pointer flex items-center justify-center w-full h-full">
-                  <input type="file" name="productImage" onChange={handleImageChange} className="hidden" />
-                  <span className="text-gray-500">Add Image</span>
-                </label>
-              )}
-            </div>  */}
-          </div>
-          <div className="flex space-x-4">
-            <div className="flex-1">
-              <label className="block text-gray-700">Product Height(cm):</label>
-              <input
-                type="number"
-                name="height"
-                value={formData.height}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
-                autoComplete="off"
-                required
-              />
-            </div>
-            <div className="flex-1">
-              <label className="block text-gray-700">Product Width(cm):</label>
-              <input
-                type="number"
-                name="width"
-                value={formData.width}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
-                autoComplete="off"
-                required
-              />
-            </div>
-          </div>
-          <div className="flex items-center space-x-2">
-            <label className="block text-gray-700">Quantity:</label>
-            <button
-              type="button"
-              onClick={handleDecrement}
-              className="px-2 py-1 bg-gray-200 text-gray-700 rounded-md focus:outline-none"
-            >
-              -
-            </button>
-            <span>{formData.quantity}</span>
-            <button
-              type="button"
-              onClick={handleIncrement}
-              className="px-2 py-1 bg-gray-200 text-gray-700 rounded-md focus:outline-none"
-            >
-              +
-            </button>
-          </div>
-          <div>
-            <label className="block text-gray-700">Shelf:</label>
-            <select 
-              type="number" 
-              name="shelf" 
-              value={formData.shelf || ''} 
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
-              autoComplete="off"
-              required
-              >
-              <option value="" disabled hidden>Select a shelf</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-            
-          </div>
-          <div>
-            <label className="block text-gray-700">Section:</label>
-            <select 
-              type="number" 
-              name="section" 
-              value={formData.section || ''} 
-              onChange={handleChange}
-              className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
-              autoComplete="off"
-              required
-              >
-              <option value="" disabled hidden>Select a section</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-            </select>
-            
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            Place Product
-          </button>
-        </form>
-      </div>
-    <div className="w-3/5 bg-white shadow-md rounded p-6 border border-gray-300 overflow-auto">
-      <Planogram
-          products={products}
-          locations={locations}
-          scalingFactorHeight={scalingFactorHeight}
-          scalingFactorWidth={scalingFactorWidth}
-        />
+          } />
+        </Routes>
       </div>
     </div>
+  </Router>
   );
 };
 
